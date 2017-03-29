ba286405d8aa073469e81c228933626244294dba
Alex <awein@umail.ucsb.edu>
2017-03-29 15:29:10 -0700

    Added in core functionality

--- tfidf.py
+++ tfidf.py
@@ -5,6 +5,53 @@ import re
 import collections
 import time
 import pprint
+import string
+
+def makeDocs(filename):
+	stop = ["a","br","about","above","after","again","against","all","am","an","and","any","are","arent","as","at","be","because","been","before","being","below","between","both","but","by","cant","cannot","could","couldnt","did","didnt","do","does","doesnt","doing","dont","down","during","each","few","for","from","further","had","hadnt","has","hasnt","have","havent","having","he","hed","hell","hes","her","here","heres","hers","herself","him","himself","his","how","hows","i","id","ill","im","ive","if","in","into","is","isnt","it","its","its","itself","lets","me","more","most","mustnt","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shant","she","shed","shell","shes","should","shouldnt","so","some","such","than","that","thats","the","their","theirs","them","themselves","then","there","theres","these","they","theyd","theyll","theyre","theyve","this","those","through","to","too","under","until","up","very","was","wasnt","we","wed","well","were","weve","were","werent","what","whats","when","whens","where","wheres","which","while","who","whos","whom","why","whys","with","wont","would","wouldnt","you","youd","youll","youre","youve","your","yours","yourself","yourselves","movie","film","see"]
+	text = open(filename, "r")
+	lines = text.read().splitlines()
+	text.close()
+	i = 0
+	docs = []
+	while i < len(lines):
+		if string.count(lines[i],"@facebook") > 1:
+			i += 1
+			"""
+			skip the line and begin a new document
+			"""
+			temp_doc = []
+			while i < len(lines) and string.count(lines[i],"@facebook") <= 1:
+				if "@facebook" not in lines[i]:
+					words = [w for w in lines[i].lower().split() if w not in stop]
+					for w in words:
+						temp_doc.append(w)
+				i += 1
+			docs.append(temp_doc)
+		i += 1
+	for d in docs:
+		num_words = len(d)
+		d = collections.Counter(d)
+		d["totalCount"] = num_words
+
+	return docs
+
+
+def tfIdf(word,doc,docs):
+	"""
+	Does the TfIdf math
+	"""
+	tf = doc[word]/doc["totalCount"]
+	docsContaining = sum(1 for d in docs if word in d)
+	idf = math.log(len(docs) / (1 + docsContaining))
+
+	return tf*idf
+
+def scoreDoc(doc,doclist):
+	scores = {word: tfIdf(word,doc,doclist) for word in doc}
+	del scores["totalCount"]
+	sorted_words = sorted(scores.items(), key = lambda t: t[1])
+	return sorted_words
 
 def buildWords(filename):
 	stop = ["a","br","about","above","after","again","against","all","am","an","and","any","are","arent","as","at","be","because","been","before","being","below","between","both","but","by","cant","cannot","could","couldnt","did","didnt","do","does","doesnt","doing","dont","down","during","each","few","for","from","further","had","hadnt","has","hasnt","have","havent","having","he","hed","hell","hes","her","here","heres","hers","herself","him","himself","his","how","hows","i","id","ill","im","ive","if","in","into","is","isnt","it","its","its","itself","lets","me","more","most","mustnt","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shant","she","shed","shell","shes","should","shouldnt","so","some","such","than","that","thats","the","their","theirs","them","themselves","then","there","theres","these","they","theyd","theyll","theyre","theyve","this","those","through","to","too","under","until","up","very","was","wasnt","we","wed","well","were","weve","were","werent","what","whats","when","whens","where","wheres","which","while","who","whos","whom","why","whys","with","wont","would","wouldnt","you","youd","youll","youre","youve","your","yours","yourself","yourselves","movie","film","see"]
@@ -31,29 +78,15 @@ def buildWords(filename):
 	We now have a list of all the words, duplicates included
 	But return a counted dict of the words
 	"""
-	return collections.Counter(words), lines
+	num_words = len(words)
+	words = collections.Counter(words)
+	words["totalCount"] = num_words
+
+	return words
 
 
-""""
-Drunk coding resulted in the following code
-"""
 
-def rankWord(word,words):
-	"""
-	Math to rank words
-	O(n^2) .... FIX
-	"""
-	wordtf = {}
-	for w in words:
-		wordtf[w] = 0
-		tf = words[w]/len(words)
-		rel_tf = 0
-		for e in words:
-			rel_tf += tf/(words[e])/len(words)
-			wordtf[e] = (e,rel_tf)
-	return wordtf
-
-def buildPhrases(lines,words):
+def buildPhrases(lines,words,word_count):
 	phrases = []
 	"""
 	For each phrase
@@ -64,20 +97,25 @@ def buildPhrases(lines,words):
 		Rank every word in the phrase according to our metric
 		"""
 		for j in i:
-			rank += rankWord(j)
+			rank += rankWord(j,words,word_count)
 		phrases.append(i,rank)
 	"""
 	return an ordered list of the most important phrases
 	"""
-	return phrases.sort(key=lambda tup: tup[1]
+	return phrases.sort(key=lambda tup: tup[1])
 
+docs = makeDocs(sys.argv[1])
 
+common_words = buildWords(sys.argv[2])
 
-common_words,lines = buildWords(sys.argv[1])
+top_words = scoreDoc(common_words,docs)
 
-phrases = buildPhrases(lines,words)
+print common_words.most_common(25)
 
-pprint.pprint(common_words.most_common(100))
+i = 1
+for word, score in top_words[:25]:
+	print "{0} -- {1} -- {2}".format(i,word,round(score,5))
+	i += 1
 
 
 
